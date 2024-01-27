ec7b437afa27b6f5d60344e2a70d047a617732e8
BlazeDrake <blaze.drake@outlook.com>
2024-01-27 08:36:32 -0700

    Minigame prefab

---
+++ Assets/Scripts/Minigames/Run/RunPlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunPlayerController : MonoBehaviour
+{
+    public MinigameController controller;
+    public float timeAlive;
+    public float playerSpeed;
+    public float strafeSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var movementDir = new Vector3(0, 0, Time.deltaTime * playerSpeed);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movementDir.x += strafeSpeed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            movementDir.x -= strafeSpeed * Time.deltaTime;
+        }
+        transform.position += movementDir;
+        timeAlive += Time.deltaTime;
+    }
+}
